be0096df980e7ebe1b398d729b9fbe7bb185fa0d
Lev Eidelman Nagar <lev@techsee.me>
2015-10-19 15:55:34 +0300

    further changes

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var app = require ('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function (argument) {
-	console.log('listening on port 3000');
+io.on('connect', function (socket) {
+	console.log('a user connected');
+
+	socket.on('disconnect', function () {
+		console.log('a user disconnected');
+	});
+});
+
+http.listen(8585, function (argument) {
+	console.log('listening on port 8585');
 });
